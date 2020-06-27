9c7fd5d30abe5c54a710bf185f009c85c136ea9c
Alejandro Orozco Builes <aorozcobuiles@gmail.com>
2020-06-27 16:15:27 -0500

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Alejandro Orozco Builes on 27/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Alejandro Orozco Builes on 27/06/20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
